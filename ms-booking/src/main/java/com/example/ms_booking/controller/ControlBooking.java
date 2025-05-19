c52a716d226882945e33f41347968e4094bfd0a8
Lina-Lpz <catalina.lopez.a@usach.cl>
2025-05-19 00:52:59 -0400

    C1.6

--- ms-booking/src/main/java/com/example/ms_booking/controller/ControlBooking.java
+++ ms-booking/src/main/java/com/example/ms_booking/controller/ControlBooking.java
@@ -1,7 +1,11 @@
 package com.example.ms_booking.controller;
 
+import com.example.ms_booking.entity.Booking;
 import com.example.ms_booking.service.ServiceBooking;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class ControlBooking {
     @Autowired
     private ServiceBooking serviceBooking;
+
+    @PostMapping("/save")
+    public ResponseEntity<Booking> saveBooking(@RequestBody Booking booking) {
+        serviceBooking.saveBooking(booking);
+        return ResponseEntity.ok(booking);
+    }
 }

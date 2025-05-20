d438b841f3973bce2f94e000edbb08ecba83fadc
Lina-Lpz <catalina.lopez.a@usach.cl>
2025-05-19 20:20:50 -0400

    C2

--- ms-booking/src/main/java/com/example/ms_booking/MsBookingApplication.java
+++ ms-booking/src/main/java/com/example/ms_booking/MsBookingApplication.java
@@ -2,10 +2,8 @@ package com.example.ms_booking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableFeignClients
 public class MsBookingApplication {
 
 	public static void main(String[] args) {

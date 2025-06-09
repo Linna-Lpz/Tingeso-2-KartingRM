4ac55af6e77f91a6360127b453dc4d35aa3eabed
Lina-Lpz <catalina.lopez.a@usach.cl>
2025-06-08 23:41:14 -0400

    Cambios en los puertos

--- kartingFrontend/src/services/services.management.js
+++ kartingFrontend/src/services/services.management.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const BOOKING_API_URL = "http://localhost:8001/booking";
-const CLIENT_API_URL = "http://localhost:8001/client";
-const VOUCHER_API_URL = "http://localhost:8001/voucher";
-const RACK_API_URL = "http://localhost:8004/rack";
-const REPORT_API_URL = "http://localhost:8006/reports";
+const BOOKING_API_URL = "http://gateway-service-service:8001/booking";
+const CLIENT_API_URL = "http://gateway-service-service:8001/client";
+const VOUCHER_API_URL = "http://gateway-service-service:8001/voucher";
+const RACK_API_URL = "http://gateway-service-service:8004/rack";
+const REPORT_API_URL = "http://gateway-service-service:8006/reports";
 
 // ------------------ Booking ------------------
 function saveBooking(data) {
